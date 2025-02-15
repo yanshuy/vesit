753a22b6df4d19fcac09bcb9b656f2bc90b3f623
yanshuy <yanshuman2005@gmail.com>
2025-02-15 15:58:35 +0530

    remove unused 3D model files; add PWA support with service worker and update app metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,66 @@
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
+    plugins: [
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: "autoUpdate",
+            strategies: "injectManifest",
+            srcDir: "src",
+            filename: "sw.ts",
+            injectManifest: {
+                swSrc: "src/sw.ts",
+                swDest: "dist/sw.js",
+            },
+            manifest: {
+                scope: "/",
+                name: "Smapa",
+                short_name: "Smapa",
+                theme_color: "#ffffff",
+                background_color: "#ffffff",
+                icons: [
+                    {
+                        src: "pwa-64x64.png",
+                        sizes: "64x64",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "maskable-icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+                start_url: "/",
+                display: "standalone",
+                orientation: "portrait",
+            },
+        }),
+    ],
+    server: {
+        allowedHosts: true,
+    },
+    preview: {
+        allowedHosts: ["9288-103-163-66-220.ngrok-free.app"],
+    },
 });
