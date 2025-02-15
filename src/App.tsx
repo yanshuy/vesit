7d4b83297ac00ec3429d1b89a8d766bd7bd83589
yanshuy <yanshuman2005@gmail.com>
2025-02-15 10:39:36 +0530

    baseurl

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,25 @@ import CalendarPage from "./pages/calendar/page";
 import ParkingSpotList from "./pages/parkingSpot/page";
 import ParkingSpotProfile from "./pages/parkingProfile/page";
 
+export const BASE_URL = "https://natural-ape-severely.ngrok-free.app";
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route path="events/:id" element={<EventPage />} />
-          <Route path="calendar" element={<CalendarPage />} />
-          <Route path="parking-spots" element={<ParkingSpotList />} />
-          <Route path="parking-spots/:id" element={<ParkingSpotProfile />} />
-          <Route path="*" element={<h1>Not Found</h1>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route path="events/:id" element={<EventPage />} />
+                    <Route path="calendar" element={<CalendarPage />} />
+                    <Route path="parking-spots" element={<ParkingSpotList />} />
+                    <Route
+                        path="parking-spots/:id"
+                        element={<ParkingSpotProfile />}
+                    />
+                    <Route path="*" element={<h1>Not Found</h1>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
